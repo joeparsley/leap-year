7a8c662d59dd646c8e1f0196c5288963da1da8fc
Joe Parsley and Tyler Allen <tyler@>
2017-05-03 08:58:22 -0700

    create leap year directory and corresponding files.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//Business Logic (Back-End)
+var leapYear = function(year) {
+  if (((year % 4 === 0) && (year % 100 !=== 0)) || (year % 400 === 0)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+
+//User Interface Logic (Front-End)
+$(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+
+});
